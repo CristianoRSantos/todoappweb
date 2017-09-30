95963b8aa23d648fd85aed251baf91d0eb293294
DesenvolvedorJava <DesenvolvedorJava@192.168.201.128>
2017-09-29 21:43:28 -0300

    commit inicial

---
+++ src/main/java/todoappweb/app/AppInit.java
@@ -0,0 +1,29 @@
+package todoappweb.app;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	public AppInit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		
+		return new Class[]{AppConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		
+		return new String[]{"/"};
+	}
+
+}
